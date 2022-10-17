1800d43aed751d73ab80504a1cb70d3a396ea520
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-17 19:07:23 -0300

    feat: req 12 - updates ingredients list to ADD 'ketchup' on every sandwich BESIDES 'McChicken' THEN shows the updated list

---
+++ challenges/desafio12.js
@@ -0,0 +1,10 @@
+db.produtos.updateMany(
+  { nome: { $nin: ["McChicken"] } },
+  {
+    $addToSet: {
+      ingredientes: "ketchup",
+    },
+  },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
