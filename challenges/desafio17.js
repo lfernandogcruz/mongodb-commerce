76f0a8c66f30c8c78e65810b36ce5e2813320248
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-17 22:26:31 -0300

    style: clean up commented lines

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,18 +1,8 @@
-// 17 - Retorne a quantidade total de produtos em
-// uma nova coleção chamada resumoProdutos
-
-// Crie uma query que insira na coleção 'resumoProdutos' um documento
-// com os campos: 'franquia' com o valor 'McDonalds' e 'totalProdutos'
-// com o valor sendo a quantidade total de produtos registrados na coleção 'produtos'.
-
 db.resumoProdutos.insertMany(
   [{
     franquia: "McDonalds",
     totalProdutos: db.produtos.count({}),
   }],
 );
-
-// Crie uma query que retorne os campos 'franquia' e o 'totalProdutos' da
-// coleção 'resumoProdutos', resultantes da primeira query.
 
 db.resumoProdutos.find({}, { _id: 0, franquia: 1, totalProdutos: 1 });
